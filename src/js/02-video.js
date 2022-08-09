b259a44b0ff92f2355ba3ca08fd85dc4bc2486d4
Olena Yatseniuk <elena.yatsenyuk@gmail.com>
2022-08-09 20:06:09 +0300

    done 2 and 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new vimeo(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+onRestartPage();
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
+
+function onRestartPage() {
+  const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let feedback = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+loadSavedMessage();
+
+function onFormInput(event) {
+  feedback[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const savedFeedback = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedFeedback) {
+    const parsedFeedback = JSON.parse(savedFeedback);
+    console.log(parsedFeedback);
+  }
+  event.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function loadSavedMessage() {
+  const savedFeedback = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (!savedFeedback) {
+    console.log('no save');
+    return;
+  }
+
+  // upload value to form
+  feedback = JSON.parse(savedFeedback);
+  if (feedback.email) {
+    refs.email.value = feedback.email;
+  }
+  if (feedback.message) {
+    refs.message.value = feedback.message;
+  }
+}
